5c57a80a58d5109e225179aa95c84566b33f083d
Errol <errolriccolin@foxmail.com>
2022-06-19 16:25:56 +0800

    Add CourseInput component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react';
+
+import CourseInput from './components/CourseGoals/CourseInput/CourseInput';
 
 import './App.css';
 
 function App() {
-  return <div></div>;
+  const [courseGoals, setCourseGoals] = useState(
+    {
+      id: 'g1',
+      text: 'Do all exercises!',
+    },
+    {
+      id: 'g2',
+      text: 'Finish the course!',
+    }
+  );
+
+  const addGoalHandler = (enteredText) => {
+    setCourseGoals((prevGoals) => {
+      const updatedGoals = [...prevGoals];
+      updatedGoals.unshift({ text: enteredText, id: Math.random().toString() });
+      return updatedGoals;
+    });
+  };
+
+  return (
+    <div>
+      <section id='goal-form'>
+        <CourseInput onAddGoal={addGoalHandler} />
+      </section>
+      <section id='goals'></section>
+    </div>
+  );
 }
 
 export default App;
